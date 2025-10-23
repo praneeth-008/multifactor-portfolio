edbbc727808cc7ad6370a7d4f6e20068dd2c4c7b
Praneeth813 <praneethjirs@gmail.com>
2025-10-23 00:28:19 -0400

    Added momentum, value, quality, and volatility factor classes

--- src/factors.py
+++ src/factors.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-class momentum12m:
+class momentum12m:    # momentum is calculated by
     def __init__(self, calculated_days = 252, skip_days= 21):
         self.calculated_days = claculated_days
         self.skip_days = skip_days
@@ -13,6 +13,46 @@ class momentum12m:
 
 momentum = r252 - r21
 return momentum
+
+class value:   # to find the value of a company we use pe ratio
+    def __init__(self, metric="PE"):
+        self.metric = metric
+
+    def score(self, fundamentals):
+        if self.metric not in fundamentals:
+            print (f" '{self.metric}' not found")
+            return pd.Series(dtype=float)
+        PE = fundamentals[self.metric].astype(float)
+
+        value_score = 1/PE
+        return value_score.sort_values(ascending=False)
+class quality:         #for quality score we find the mean of net income on equity and assets
+    def __init__(self, metric=["ROE","ROA"]):
+        self.metric = metric
+    def score(self, fundamentals):
+        if self.metric not in fundamentals:
+            print (f" '{self.metric}' not found")
+            return pd.Series(dtype=float)
+
+        roe = fundamentals["ROE"].astype(float)
+        roa = fundamentals["ROA"].astype(float)
+        quality_score = (roe+roa)/2
+        return quality_score.sort_values(ascending=False)
+
+class volatality:
+    def __init__(self, calculated_days = 252):
+        self.calculated_days = calculated_days
+
+    def score(self, price):
+
+        returns = price.pct_change().dropna()
+        volatility = returns.std()
+        annual_volatility = volatility * np.sqrt(self.calculated_days)
+        vol_score = 1/annual_volatility
+        return vol_score.sort_values(ascending=False)
+
+
+
 
 
 
