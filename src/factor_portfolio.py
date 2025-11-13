5a3d367bf0f83d2363bf708a77ed40d3d61264e0
Praneeth813 <praneethjirs@gmail.com>
2025-11-12 19:33:28 -0500

    updated complete factor portfolio

--- src/factor_portfolio.py
+++ src/factor_portfolio.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 
 class factor_portfolio:
-    def __init__(self, name, returns, factor_scores, top_n = 25, weights = None):
+    def __init__(self, name, prices, factor_scores, top_n = 25, weights = None):
         self.name = name
-        self.returns = returns
+        self.prices = prices
         self.factor_scores = factor_scores
         self.top_n = top_n
         self.weights = weights
@@ -14,5 +14,30 @@ class factor_portfolio:
         self.weights = pd.series(1/self.top_n , index = top_stocks.index)
         return self.weights
 
-    def returns(self):
+    def compute_returns(self):
+        if self.weights is not None:
+            raise ValueError("Portfolio weights not defined. Run build() first.")
+        top_prices = self.prices[self.weigths.index]
+        returns = top_prices.pct_change().dropna()
+        self.portfolio_returns = (returns*self.weights).sum(axis = 1)
+        return self.portfolio_returns
+
+    def characterstics(self, rf = 0 , freq = 252):
+        if self.portfolio_returns is None:
+            raise ValueError("No portfolio returns found. Run compute_returns() first.")
+        mean_return = self.portfolio_returns.mean()*freq
+        volatality = self.portfolio_returns.std() * np.sqrt(freq)
+        sharpe = (mean_return-rf)/volatality
+
+        cumulative = (1 + self.portfolio_returns).cumprod()
+        drawdown = cumulative / cumulative.cummax() - 1
+        max_drawdown = drawdown.min()
+
+        return {
+            'Portfolio': self.name,
+            'Annual Return': mean_return,
+            'Volatility': volatility,
+            'Sharpe Ratio': sharpe,
+            'Max Drawdown': max_drawdown
+        }
 
